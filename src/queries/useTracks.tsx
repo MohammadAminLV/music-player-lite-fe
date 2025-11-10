14fdbd973e974e807b66e8b124f74ea41c8d3107
MohammadAmin Alavi <mohammadaminlv@yahoo.com>
2025-11-11 00:28:56 +0330

    change types

--- src/queries/useTracks.tsx
+++ src/queries/useTracks.tsx
@@ -2,24 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import { Track } from "../types";
 import { API as API_ROUTE } from "../lib/environment";
 
-export type Payload = {
-    data: Track[]
+export type TracksResponse = {
+  data: Track[];
 };
 
-export async function fetchData(): Promise<Payload> {
-    const res = await fetch(`${API_ROUTE}/api/tracks`);
-    if (!res.ok) {
-        throw new Error(`Failed to fetch tracks: ${res.status}`);
-    }
-    const json = await res.json();
-    return json as Payload;
+export async function fetchTracks(): Promise<TracksResponse> {
+  const res = await fetch(`${API_ROUTE}/api/tracks`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch tracks: ${res.status}`);
+  }
+  const json = (await res.json()) as TracksResponse;
+  return json;
 }
 
 export default function useTracks() {
-    return useQuery({
-        queryKey: ["tracks"],
-        queryFn: fetchData,
-        suspense: true,
-        staleTime: 1000 * 60 * 2
-    });
+  return useQuery<TracksResponse, Error>({
+    queryKey: ["tracks"],
+    queryFn: fetchTracks,
+    suspense: true,
+    staleTime: 1000 * 60 * 2,
+  });
 }
