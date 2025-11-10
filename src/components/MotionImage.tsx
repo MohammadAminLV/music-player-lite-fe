4426e52f907c763ef72b5575c43391b7d53fff5b
MohammadAmin Alavi <mohammadaminlv@yahoo.com>
2025-11-11 00:13:25 +0330

    type fixes

--- src/components/MotionImage.tsx
+++ src/components/MotionImage.tsx
@@ -1,22 +1,24 @@
 "use client";
 
-import { motion } from "framer-motion";
-import Image from "next/image";
+import { motion, MotionProps } from "framer-motion";
+import Image, { ImageProps } from "next/image";
 import { useAudio } from "./AudioProvider";
 
-const MotionImageComponent = motion(Image);
+const MotionImageComponent = motion<ImageProps>(Image);
 
-export const MotionImage: React.FC = (props) => {
-    const { state } = useAudio();
+type MotionImageProps = ImageProps & MotionProps;
 
-    if (!state.track) return null;
+export const MotionImage: React.FC<MotionImageProps> = (props) => {
+  const { state } = useAudio();
 
-    return (
-        <MotionImageComponent
-            initial={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.28 }}
-            {...props}
-        />
-    );
+  if (!state.track) return null;
+
+  return (
+    <MotionImageComponent
+      initial={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.28 }}
+      {...props}
+    />
+  );
 };
